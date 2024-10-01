3d7c35aae60bb9ecdc7b7ee7a8a00e93a1e47df7
csanalosibalint <csanalosib@kkszki.hu>
2024-10-01 14:47:01 +0200

    Adatok kiiratasa

--- ProductApi/obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
+++ ProductApi/obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46c475acadbc46a3177897396b185565f4a71d7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f151ce526c04c0cf225ef3398bf151ecad07b81")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
